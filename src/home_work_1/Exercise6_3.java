de3538716e6a822de5704e3dae7a769e0c3c5da4
Alekseevich29 <werjal91@gmail.com>
2024-03-05 13:40:16 +0300

    Удаление папок

---
+++ src/home_work_1/Exercise6_3.java
@@ -0,0 +1,20 @@
+package home_work_1;
+
+import java.util.Scanner;
+
+public class Exercise6_3 {
+    public static void main(String[] agr){
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите ваше имя!");
+        String name = console.nextLine();
+        switch (name){
+            case "Вася":
+                System.out.printf("Привет \nЯ тебя так долго ждал");
+                break;
+            case "Анастасия":
+                System.out.printf("Я тебя так долго ждал");
+                break;
+            default:System.out.println("Добрый день, а вы кто?");
+        }
+    }
+}
